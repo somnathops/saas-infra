d19c666e0b1e3b7305b3d07c03956a64a6cbf61e
Somnath Manna <somnath.manna@Somnaths-MacBook-Air.local>
2026-07-29 22:46:47 +0530

    Fix the CI error

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_name         = var.vpc_name
-  vpc_cidr         = var.vpc_cidr
-  public_subnets   = var.public_subnets
-  private_subnets  = var.private_subnets
+  vpc_name        = var.vpc_name
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
   # nat_gateway_mode = var.nat_gateway_mode
-  common_tags      = var.common_tags
+  common_tags = var.common_tags
 }
