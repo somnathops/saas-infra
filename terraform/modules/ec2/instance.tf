e3728f0af5790f372ba8e7a9d78771b7411bb70d
Somnath Manna <somnath.manna@Somnaths-MacBook-Air.local>
2026-08-19 02:02:00 +0530

    Fix the root module errors

--- terraform/modules/ec2/instance.tf
+++ terraform/modules/ec2/instance.tf
@@ -1,10 +1,9 @@
 resource "aws_instance" "this" {
-    ami                    = var.ami_id
-    instance_type          = var.instance_type
-    subnet_id              = var.subnet_id
-    vpc_security_group_ids = var.security_group_ids
-    key_name               = var.key_name
-    associate_public_ip_address = var.associate_public_ip_address
-    tags = var.tags
-  
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.key_name
+  associate_public_ip_address = var.associate_public_ip_address
+  tags                        = var.tags
+
 }
--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,35 +1,32 @@
 variable "ami_id" {
-    description = "The AMI ID to use for the EC2 instance."
-    type        = string
+  description = "The AMI ID to use for the EC2 instance."
+  type        = string
 }
 
 variable "instance_type" {
-    description = "The type of instance to use for the EC2 instance."
-    type        = string
+  description = "The type of instance to use for the EC2 instance."
+  type        = string
 }
 
 variable "subnet_id" {
-    description = "The subnet ID to launch the EC2 instance in."
-    type        = string
-}
-
-variable "security_group_ids" {
-    description = "A list of security group IDs to associate with the EC2 instance."
-    type        = list(string)
+  description = "The subnet ID to launch the EC2 instance in."
+  type        = string
 }
 
 variable "key_name" {
-    description = "The name of the key pair to use for the EC2 instance."
-    type        = string
+  description = "The name of the key pair to use for the EC2 instance."
+  type        = string
 }
 
 variable "associate_public_ip_address" {
-    description = "Whether to associate a public IP address with the EC2 instance."
-    type        = bool
-    default     = false
+  description = "Whether to associate a public IP address with the EC2 instance."
+  type        = bool
+  default     = false
 }
 
 variable "tags" {
-    description = "A map of tags to assign to the EC2 instance."
-    type        = map(string)
+  description = "A map of tags to assign to the EC2 instance."
+  default = {
+    ManagedBy = "Terraform"
+  }
 }
